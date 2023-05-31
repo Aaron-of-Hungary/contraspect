1ab9bce34be1c56aeeb57326a10b976b69eee1dc
Aaron-of-Hungary <hajdu.moharos.aron@gmail.com>
2023-05-31 07:02:03 +0200

    contraspect-5th-commit. rewrote-source-files.

--- src/.#B3.cpp
+++
@@ -1,1 +0,0 @@
-george@george-HP-250-G5-Notebook-PC.3875:1685478135
--- src/Dn.cpp
+++ src/Dn.cpp
@@ -1,39 +1,33 @@
-/*ROS listener.cpp - ~/catkin_ws/src/contraspect/src/Dn.cpp
-*/
-
 /*
- * Author: Anis Koubaa for Gaitech EDU
- * Year: 2016
- *
+
+ROS Dn.cpp - ~/catkin_ws/src/contraspect/src/Dn.cpp
+
+Author: 
+ Hajdu-Moharos Áron
+Project: 
+ Önálló Laboratórium 2022/23/2
+ ContraSpect Drón-lokációs- és -navigációs rendszer
+
  */
 
 #include "ros/ros.h"
 #include "std_msgs/String.h"
 
-// Topic messages callback
-void chatterCallback(const std_msgs::String::ConstPtr& msg)
-{
-    ROS_INFO("[Dn] I heard: [%s]\n", msg->data.c_str());
+void triangCallback(const std_msgs::String::ConstPtr& msg){
+    ROS_INFO("[Dn] Msg from topic triang: [%s]\n", msg->data.c_str());
 }
 
-int main(int argc, char **argv)
-{
-    // Initiate a new ROS node named "listener"
-	ros::init(argc, argv, "Dn_node");
-	//create a node handle: it is reference assigned to a new node
-	ros::NodeHandle node;
-
-
-    // Subscribe to a given topic, in this case "chatter".
-	//chatterCallback: is the name of the callback function that will be executed each time a message is received.
-    ros::Subscriber sub = node.subscribe("triang", 1000, chatterCallback);
-
-    // Enter a loop, pumping callbacks
+int main(int argc, char **argv){
+    ros::init(argc, argv, "Dn_node");
+    ros::NodeHandle node;
+    ros::Subscriber sub = node.subscribe("triang", 1000, triangCallback);
     ros::spin();
-
     return 0;
 }
 
-/* Exit
-/media/george/3218A8F718A8BAED/Users/hajdu/BME/ONL/ROS-projects 
+/* 
+
+Exit
+ /media/george/3218A8F718A8BAED/Users/hajdu/BME/ONL/ROS-projects 
+
 */
